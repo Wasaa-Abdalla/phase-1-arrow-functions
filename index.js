d7a602d6092849648dc22e5cd9fb3e1fafa984f6
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-07-22 07:57:30 +0300

    Completed

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(num1,num2){
+    let result = num1/num2;
+    console.log(result);
+}
+const square = x => console.log(x**2);
+const add = (x,y) => console.log(x+y);
+
+divide(2000,100);
+square(2);
+add(5,2);
